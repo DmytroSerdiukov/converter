f0469c09b9d6213b9bd805f0efecdd16a36af4ad
Jon1k <zenaserdykov2004@gmail.com>
2023-01-06 21:08:32 +0200

    commit

--- src/api/index.js
+++ src/api/index.js
@@ -1,7 +1,6 @@
 import axios from 'axios'
 
 const Currency = axios.create({
-    // baseURL: 'https://api.exchangeratesapi.io/latest',
     baseURL: 'https://api.apilayer.com/exchangerates_data',
     headers: {
         'apikey': 'ULEUbfi9ZZoAnTEvIinQHbVG1boiJ1yG',
